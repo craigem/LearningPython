d795b5613e8d3cd7d71080daaea7efe782e4f256
Craige McWhirter <craige@mcwhirter.com.au>
2015-07-04 19:05:56 +1000

    Completed script1

---
+++ script1.py
@@ -0,0 +1,7 @@
+'''First Python script'''
+
+import sys              # Load a library module
+print sys.platform
+print 2 ** 100          # Raise to a power of 2
+X = 'Spam!'
+print X * 8             # String repetition
